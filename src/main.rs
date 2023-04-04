74a5b9fa38e6451b2a246a2708bb974390b42442
xing-6464 <xing_6464@foxmail.com>
2023-04-04 19:00:08 +0800

    元组

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,21 @@
 // 常量： const 关键字
 // Shadowing: 隐藏
 
-fn avg(a: u32, b: u32) -> u32 {
-    (a & b) + ((a ^ b) >> 1)
-}
-
 
 fn main() {
-    let a: u32 = "4294967295".parse::<u32>().unwrap();
-    let b: u32 = 2;
+    let a: i32 = 10;
+    let b: char = 'A';
 
-    let (mul, is_overflow) = a.overflowing_mul(b);
-    println!("mul = {}, is_overflow = {}", mul, is_overflow);
+    let mytuple = (a, b);
 
+    let (c,d) = mytuple;
 
-    assert_eq!(avg(4294967295, 4294967295), 4294967295);
-    assert_eq!(avg(0, 0), 0);
-    assert_eq!(avg(10, 20), 15);
-    assert_eq!(avg(4294967295, 1), 2147483648);
-    println!("passed");
+    println!("c = {}, d = {}", c, d);
+    println!("mytuple.0 = {}", mytuple.0);
+    println!("mytuple.1 = {}", mytuple.1);
+
+    let (res, is_overflow) = a.overflowing_add(10);
+
+    println!("{} {}", res, is_overflow);
+
 }
