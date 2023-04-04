192f3629a1f366035e74158f87b74948fb95df80
xing-6464 <xing_6464@foxmail.com>
2023-04-05 02:52:21 +0800

    函数与方法

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
-enum Alphabet {
-    A,
-    B,
-    C
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+impl Point {
+    fn new(x: u64, y: u64) -> Point {
+        Point { x , y }
+    }
+
+    fn get_x(&self) -> u64 {
+        self.x
+    }
+
+    fn set_x(&mut self, x: u64) {
+        self.x = x
+    }
 }
 
 
-
 fn main() {
-    let letter = Alphabet::A;
+    let mut p = Point::new(10, 20);
+    println!("{:?}", p);
+    println!("{:?}", p.get_x());
 
-    match letter {
-        Alphabet::A => {
-            println!("It a");
-        }
-        _ => {
-
-        }
-    }
-
-    if let Alphabet::A = letter {
-        println!("It a");
-    }
+    p.set_x(20);
+    println!("{:?}", p.get_x());
 }
