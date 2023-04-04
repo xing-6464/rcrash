e2c474f85f56bbdfda40e11997cfc5076b7b2dac
xing-6464 <xing_6464@foxmail.com>
2023-04-04 18:54:45 +0800

    处理overflow

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,22 @@
 // 常量： const 关键字
 // Shadowing: 隐藏
 
+fn avg(a: u32, b: u32) -> u32 {
+    (a & b) + ((a ^ b) >> 1)
+}
+
 
 fn main() {
-    let c = 'z';
-    let z = 'Z';
-    let heart_eyed_cat = '中';
+    let a: u32 = "4294967295".parse::<u32>().unwrap();
+    let b: u32 = 2;
 
-    println!("{} {} {}", c,z,heart_eyed_cat);
+    let (mul, is_overflow) = a.overflowing_mul(b);
+    println!("mul = {}, is_overflow = {}", mul, is_overflow);
+
+
+    assert_eq!(avg(4294967295, 4294967295), 4294967295);
+    assert_eq!(avg(0, 0), 0);
+    assert_eq!(avg(10, 20), 15);
+    assert_eq!(avg(4294967295, 1), 2147483648);
+    println!("passed");
 }
