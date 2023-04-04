55f94da5286cc88d048123999bb6ed37a984bd9f
xing-6464 <xing_6464@foxmail.com>
2023-04-04 19:05:41 +0800

    数组

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
-// 创建变量：let 关键字
-// 变量默认不开变
-// 可变变量：变量名称加关键字 mut
-// 常量： const 关键字
-// Shadowing: 隐藏
-
-
 fn main() {
-    let a: i32 = 10;
-    let b: char = 'A';
+    let myarray: [u32; 5] = [1, 2, 3, 4, 5];
+    
+    println!("myarray[1]={}", myarray[1]); // 2
 
-    let mytuple = (a, b);
+    let mybuffer: [u32; 32 * 1024] = [0; 32 * 1024];
 
-    let (c,d) = mytuple;
+    println!("mybuffer[1024] = {}", mybuffer[1024]);
 
-    println!("c = {}, d = {}", c, d);
-    println!("mytuple.0 = {}", mytuple.0);
-    println!("mytuple.1 = {}", mytuple.1);
-
-    let (res, is_overflow) = a.overflowing_add(10);
-
-    println!("{} {}", res, is_overflow);
-
+    
 }
