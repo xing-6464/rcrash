28d9e184e9b744d11ddd3ce76e7e7cfe34295749
xing-6464 <xing_6464@foxmail.com>
2023-04-05 02:35:45 +0800

    match

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 fn main() {
-    for i in 0..5 {
-        println!("{}", i)
-    }
+    let opcode: u8 = 42;
 
-    for i in 0..=5 {
-        println!("{}", i)
-    }
-
-    let myarray = ["a", "b", "c"];
-    for i in myarray.iter() {
-        println!("{:?}", i);
-    }
-
-    let mut myarr = [1, 2, 3];
-    for i in myarr.iter_mut() {
-        *i *= 2;
-    }
-    for i in myarr.iter() {
-        println!("{}", i);
+    match  opcode {
+        42 => {
+            println!("bingo!");
+        }
+        _ => {
+            println!("{}", opcode);
+        }
     }
 }
