533e7b6a49b8c16dc626729657c214d02ac47f7c
xing-6464 <xing_6464@foxmail.com>
2023-04-05 02:58:13 +0800

    闭包

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
-#[derive(Debug)]
-struct Point {
-    x: u64,
-    y: u64,
-}
-
-impl Point {
-    fn new(x: u64, y: u64) -> Point {
-        Point { x , y }
-    }
-
-    fn get_x(&self) -> u64 {
-        self.x
-    }
-
-    fn set_x(&mut self, x: u64) {
-        self.x = x
-    }
-}
-
+use std::thread;
 
 fn main() {
-    let mut p = Point::new(10, 20);
-    println!("{:?}", p);
-    println!("{:?}", p.get_x());
 
-    p.set_x(20);
-    println!("{:?}", p.get_x());
+
+    let hello_message = "Hello World";
+
+    thread::spawn(move || {
+        println!("{}", hello_message);
+    }).join();
+
 }
