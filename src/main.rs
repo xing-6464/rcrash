9d4f4697a44446ca29a3d12a331dd84e415a2689
xing-6464 <xing_6464@foxmail.com>
2023-04-05 02:03:31 +0800

    if else

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use std::mem;
-
 fn main() {
-    unsafe {
-        let a = [0u8, 1u8, 0u8, 0u8];
-        let b: u32 = mem::transmute(a);
-        println!("{}", b)
+    let n = 5;
+
+    if n > 0 {
+        println!("{} is positive", n);
+    } else if n < 0 {
+        println!("{} is nefative", n);
+    } else {
+        println!("{} is ziro" ,n)
     }
+
+    let m = if n < 0 {
+        2.0
+    } else {
+        3.0
+    };
+
+    println!("{} value is m", m);
 }
